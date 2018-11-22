0b09e0212227cdfff6e6fc6a5c14e53c4e1eea14
Alix Taffle <ataffle@gmail.com>
2018-11-22 14:26:32 +0100

    simple form install

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Moscow Mule")
+Cocktail.create(name: "Rhum planteur")
