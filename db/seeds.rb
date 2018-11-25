ac1d1de60099cc888813e846a6f19d865856a4aa
Alix Taffle <ataffle@gmail.com>
2018-11-25 12:35:33 +0100

    add new ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.destroy_all
 
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Rhum")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Calvados")
+Ingredient.create(name: "Ginger beer")
+Ingredient.create(name: "Vermouth")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Strawberry juice")
+Ingredient.create(name: "Tequila")
 
 
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Moscow Mule")
-Cocktail.create(name: "Rhum planteur")
